cce2b1a63fd9971cd1f0368edd4bf856a55e33ea
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-02-22 16:43:44 -0300

    templates done

--- scripts/generate/src/main.rs
+++ scripts/generate/src/main.rs
@@ -2,6 +2,7 @@ use std::process::Command;
 
 fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
+    Command::new("cargo").args(["new", "--lib", "extensions/example"]);
 
     // Command
     Ok(())
